9f68f8e73ab5e88d06af98dcbf570feda077f026
Moises Gomez <moisesgomez@Juanas-MacBook-Air.local>
2023-12-24 18:23:34 -0800

    tracking webpack file

---
+++ webpack.config.js
@@ -0,0 +1,26 @@
+const path = require('path');
+
+module.exports = {
+  entry: './src/index.js',
+  output: {
+    path: path.resolve(__dirname, 'build'),
+    filename: 'bundle.js'
+  },
+  mode: 'production',
+  module: {
+    rules: [
+      {
+        test: /\.jsx?/,
+        exclude: /node_modules/,
+        use: {
+          loader: 'babel-loader',
+          options: {
+            presets: [
+              ['@babel/preset-env'], ['@babel/preset-react']
+            ]
+          }
+        }
+      }
+    ]
+  }
+};
